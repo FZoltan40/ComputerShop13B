424831958ed0ab3c03e60a46b1866b457d370b5d
FZoltan38 <farkaszoltan28@gmail.com>
2025-09-29 11:05:10 +0200

    IDatabase interface and GetData funcion is added.

---
+++ Services/IDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ComputerShop13B.Services
+{
+    internal interface IDatabase
+    {
+        ICollection<object> GetAllData();
+        object GetData(string username, string password);
+    }
+}
---
+++ Services/Users.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ComputerShop13B.Services
+{
+    internal class Users : IDatabase
+    {
+        Connect conn = new Connect();
+        public ICollection<object> GetAllData()
+        {
+            ICollection<object> data = new List<object>();
+            conn._connection.Open();
+
+
+            conn._connection.Close();
+
+            return data;
+        }
+
+        public object GetData(string username, string password)
+        {
+            conn._connection.Open();
+
+            string sql = "SELECT * FROM users WHERE UserName = @username AND Password = @password";
+
+            conn._connection.Close();
+
+            return new { message = "" };
+        }
+    }
+}

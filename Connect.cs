1bdcdbd8e9bf787e50da41aecb555534f1218562
FZoltan38 <farkaszoltan28@gmail.com>
2025-09-29 10:09:12 +0200

    Folders and files are added to build.

---
+++ Connect.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputerShop13B
+{
+    internal class Connect
+    {
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _pass;
+    }
+}
---
+++ LoginPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace ComputerShop13B
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+
+
+
+        }
+    }
+}
